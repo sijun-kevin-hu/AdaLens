e01eca1b2a4c08225ab92d47c94a49564e85db94
thekevinhu <22kevinhu@gmail.com>
2025-07-13 22:58:22 -0400

    feat: update build configuration and add static copy plugin for manifest and icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'manifest.json',
+                    dest: '.'
+                },
+                {
+                    src: './icons',
+                    dest: '.'
+                }
+            ]
+        })
+    ],
     build: {
         outDir: 'dist',
-        'rollupOptions': {
+        rollupOptions: {
             input: {
                 background: resolve(__dirname, 'src/background.ts'),
                 content: resolve(__dirname, 'src/content.ts'),
-                popup: resolve(__dirname, 'src/popup.ts'),
-                options: resolve(__dirname, 'src/options.ts')
+                options: resolve(__dirname, 'options.html'),
+                popup: resolve(__dirname, 'popup.html'),
             },
             output: {
                 entryFileNames: '[name].js',
